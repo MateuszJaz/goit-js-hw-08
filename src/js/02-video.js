02fa2dceaba293c606d07dba5b24cbd7698b77fa
MateuszJaz <mateusz.jaz@outlook.com>
2022-08-01 18:57:10 +0200

    final upload

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const iframePlayer = new Player(iframe);
+
+iframePlayer.on(
+  'timeupdate',
+  throttle(function () {
+    iframePlayer
+      .getCurrentTime()
+      .then(function (seconds) {
+        localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+        console.log('Current time:', seconds);
+      })
+      .catch(function (error) {
+        alert('Error:', error);
+      });
+  }, 1000)
+);
+
+iframePlayer
+  .setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+iframePlayer.on('pause', function () {
+  console.log('Video paused!');
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const throttle = require('lodash.throttle');
 
+const form = document.querySelector('form');
+const email = form.querySelector('input');
+const text = form.querySelector('textarea');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    const formInput = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formInput));
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  console.log({ email: email.value, message: message.value });
+
+  event.currentTarget.reset();
+  localStorage.clear();
+});
+
+const storage = localStorage.getItem(LOCALSTORAGE_KEY);
+const parsedStorageData = JSON.parse(storage);
+const tryFoo = () => {
+  if (parsedStorageData !== null) {
+    email.value = parsedStorageData.email;
+    text.value = parsedStorageData.message;
+  }
+};
+
+tryFoo();
